6538e320d19874cd3d5db8af36b1e74b5f4433a8
Justramin <143264759+Justramin@users.noreply.github.com>
2024-12-10 09:45:18 +0530

    Time: 8 ms (63.42%) | Memory: 58.9 MB (36.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -6,14 +6,6 @@ var isPalindrome = function(x) {
    let stringfy = x.toString();
   let arr = stringfy.split("");
 let reversed = arr.reverse().join('')
-//   let reversed = "";
-//   const len = arr.length;
-//   let v = arr.length - 1;
-
-//   for (let i = 0; i < len; i++, v--) {
-//     reversed += arr[v];
-//   }
-
   if (stringfy === reversed) {
     return true;
   }
