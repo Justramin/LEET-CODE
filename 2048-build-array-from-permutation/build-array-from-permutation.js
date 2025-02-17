8df39e50d2dcee00452c816ba9920e3bb3cd76f8
JUSTIN RAM <143264759+Justramin@users.noreply.github.com>
2025-02-17 09:29:19 +0530

    Time: 1 ms (78.06%) | Memory: 52.9 MB (62.54%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.js
+++ 2048-build-array-from-permutation/build-array-from-permutation.js
@@ -2,10 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var buildArray = function(nums) {
-    let out =[]
-    for(let i=0 ; i<nums.length ;i++){
-        out.push(nums[nums[i]])
-    }
-    return out
-};
+var buildArray = nums => nums.map(i => nums[i]);
