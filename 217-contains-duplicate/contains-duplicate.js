2481e7d6df10aa540fd907b3accbb592dd7b663a
JUSTIN RAM <143264759+Justramin@users.noreply.github.com>
2025-02-18 11:37:09 +0530

    Time: 15 ms (45.17%) | Memory: 60.6 MB (75.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,9 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b)=>a-b)
-    for(let i=0 ; i<nums.length ; i++){
-        if(nums[i]==nums[i+1])return true
+    const set = new Set(nums)
+    const res = Array.from(set)
+    if(nums.length=== res.length){
+        return false
+    }else{
+        return true
     }
     return false
 };
