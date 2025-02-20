0786cdcf54755c5736e38c613b5f5f21e173c584
JUSTIN RAM <143264759+Justramin@users.noreply.github.com>
2025-02-20 09:53:41 +0530

    Time: 2 ms (42.37%) | Memory: 54.9 MB (5.01%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -5,16 +5,9 @@
  */
 var isSubsequence = function(s, t) {
  let out = 0;
-    
-    for(let i=0; i<t.length; i++) {
-        if(s[out] === t[i]) {
-            out++;
-        }
-        
-        if(out === s.length) {
-            break;
-        }
-    }
-    
+for (let char of t) {
+    if (s[out] === char) out++;
+    if (out === s.length) break;
+}   
     return out === s.length
 };
